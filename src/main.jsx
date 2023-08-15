a893085150f34b368819b40015ac9d9d6fd31906
Emiliano Broggi <broggi4@hotmail.com>
2023-08-15 19:05:30 -0300

    Aplicacion app Peliculas

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style/MovieSearch.css'
+import { MovieSearchApp } from '../MovieSearchApp'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+  <MovieSearchApp/>
+
+    </React.StrictMode>,
+)
